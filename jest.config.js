a0b8115c5c4c498c9566b42f5fe08583ca6b6ed5
John Schellinger <schellingerj1@gmail.com>
2022-06-09 17:40:40 -0400

    Ignore handler path resolver in coverage

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,7 @@ const { compilerOptions } = require('./tsconfig.paths.json');
 module.exports = {
   preset: 'ts-jest',
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/{!(index|schema),}.ts'],
+  collectCoverageFrom: ['src/**/{!(index|schema|handler-resolver),}.ts'],
   coverageReporters: ['lcov', 'text-summary'],
   coverageDirectory: 'coverage',
   moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
